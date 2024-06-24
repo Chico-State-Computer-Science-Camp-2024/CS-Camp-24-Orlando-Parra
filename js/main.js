bd819cb9cc9cf856d3809275581be0bb5a205e53
orlandobedabest <thecreeperboi21@gmail.com>
2024-06-24 22:45:24 +0000

    Copied Code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
     $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
+        window.alert("!!YOU JUST GOT A VIRUS!! Press OK to intall cleaner");
+    });
+});
+$( document ).ready(function() {
+    $('#bttn').click(function() {
+        window.alert("!!YOU JUST GOT ONE MILLION DOLLARS!! Press OK to claim");
     });
 });
